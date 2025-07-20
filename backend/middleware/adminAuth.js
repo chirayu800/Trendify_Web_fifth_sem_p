07ff6b3810e45c209214dbc038ae602e94b3ce0b
chirayu Baij <chirayu200@gmail.com>
2025-07-20 23:16:17 +0545

    server.js

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,24 +1,24 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-const adminAuth = async (req, res, next) => {
-  try {
-    const { token } = req.headers;
+// const adminAuth = async (req, res, next) => {
+//   try {
+//     const { token } = req.headers;
 
-    if (!token) {
-      return res.status(401).json({ success: false, message: "Unauthorized!" });
-    }
+//     if (!token) {
+//       return res.status(401).json({ success: false, message: "Unauthorized!" });
+//     }
 
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (decodedToken !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-      return res.status(401).json({ success: false, message: "Unauthorized!" });
-    }
+//     if (decodedToken !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+//       return res.status(401).json({ success: false, message: "Unauthorized!" });
+//     }
 
-    next();
-  } catch (error) {
-    console.log("Error while authenticating admin: ", error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-};
+//     next();
+//   } catch (error) {
+//     console.log("Error while authenticating admin: ", error);
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// };
 
-export default adminAuth;
+// export default adminAuth;
