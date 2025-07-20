384ba7e3114dff0b9763227bc1059f42d69feea5
chirayu Baij <chirayu200@gmail.com>
2025-07-20 10:13:06 +0545

    user route updated

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,14 +1,14 @@
-// import express from "express";
-// import {
-//   loginUser,
-//   registerUser,
-//   loginAdmin,
-// } from "../controllers/userController.js";
+import express from "express";
+import {
+  loginUser,
+  registerUser,
+  loginAdmin,
+} from "../controllers/userController.js";
 
-// const userRouter = express.Router();
+const userRouter = express.Router();
 
-// userRouter.post("/register", registerUser);
-// userRouter.post("/login", loginUser);
-// userRouter.post("/admin", loginAdmin);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/admin", loginAdmin);
 
-// export default userRouter;
+export default userRouter;
