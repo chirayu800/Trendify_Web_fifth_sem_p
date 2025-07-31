85e04fd3db32ee3657e62c9d19998ee846ed233e
chirayu Baij <chirayu200@gmail.com>
2025-07-31 11:03:24 +0545

    cartRoute updated

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,12 +1,12 @@
-// import express from "express";
+import express from "express";
 
-// import { addToCart, getCartDetails, removeFromCart } from "../controllers/cartController.js";
+import { addToCart, getCartDetails, removeFromCart } from "../controllers/cartController.js";
 
 
-// const cartRouter = express.Router();
+const cartRouter = express.Router();
 
-// cartRouter.post("/add", addToCart);
-// cartRouter.get("/list/:userId", getCartDetails);
-// cartRouter.delete("/remove", removeFromCart);
+cartRouter.post("/add", addToCart);
+cartRouter.get("/list/:userId", getCartDetails);
+cartRouter.delete("/remove", removeFromCart);
 
-// export default cartRouter;
+export default cartRouter;
